a9cc0ac06fa4d1b907f5a27ef6d0dee85c217215
Felipe Gauer <felipe.gattelli@edu.pucrs.br>
2025-02-04 16:00:56 -0300

    datset graphs (incomplete)

--- web/back/controllers/datasetController.js
+++ web/back/controllers/datasetController.js
@@ -1,8 +1,10 @@
 const Dataset = require('../models/Dataset')
+const Exam = require('../models/Exam')
+const Pacient = require('../models/Pacient')
 
 
 const datasetController = {
-    all: async function(req, res) {
+    all: async function (req, res) {
         try {
             const datasets = await Dataset.find()
             res.json(datasets.map(d => d.name))
@@ -12,32 +14,85 @@ const datasetController = {
     },
 
 
-    info: async function(req, res) {
+    info: async function (req, res) {
         try {
             let name = req.params.dataset;
             const dataset = await Dataset.findOne({ name: name })
             res.json({
-                nome:dataset.name, 
-                inicio:dataset.inicio, 
-                fim:dataset.fim, 
-                mortos:dataset.mortos,
-                pacientes:dataset.pacientes, 
-                vivos:dataset.vivos})
+                nome: dataset.name,
+                inicio: dataset.inicio,
+                fim: dataset.fim,
+                mortos: dataset.mortos,
+                pacientes: dataset.pacientes,
+                vivos: dataset.vivos
+            })
         } catch (error) {
             res.status(404).json({ err: error })
         }
     },
 
-    stats: async function(req, res) {
+    stats: async function (req, res) {
         try {
             let name = req.params.dataset;
             const dataset = await Dataset.findOne({ name: name })
             res.json({
-                head:dataset.estatisticas.head,
-                body:dataset.estatisticas.body})
+                head: dataset.estatisticas.head,
+                body: dataset.estatisticas.body
+            })
         } catch (error) {
             res.status(404).json({ err: error })
         }
+    },
+
+    pacients_stats: async function (req, res) {
+        try {
+            let name = req.body.dataset
+            const exams = await Exam.find({ dataset: name })
+            const vivosStats = {}
+            const mortosStats = {}
+
+            let vivos = 0
+            let mortos = 0
+            await exams.forEach(p => {
+                p.exams.forEach(async e => {
+                    let soma = 0
+                    let l = 0
+                    e.data.forEach(d => {
+                        soma += d.y
+                        l++
+                    })
+                    const media = soma / l;
+                    const pacient = await Pacient.findOne({ codigo: p.codigo })
+                    if(pacient.desfecho === 'Vivo'){
+                        console.log('vivo');
+                        
+                        vivos++
+                        vivosStats[e.name] = vivosStats[e.name] ? vivosStats[e.name] + media : media
+                    }else{
+                        mortos++
+                        mortosStats[e.name] = mortosStats[e.name] ? mortosStats[e.name] + media : media
+                    }
+                    
+                });
+
+            })
+            
+            for (const key in vivosStats) {
+                vivosStats[key] = vivosStats[key] / vivos
+            }
+
+            for (const key in mortosStats) {
+                mortosStats[key] = mortosStats[key] / mortos
+            }
+
+
+
+            res.json({ vivos: vivosStats, mortos: mortosStats })
+
+        } catch (error) {
+            res.status(404).json({ err: error })
+        }
+
     }
 
 }
--- web/back/routes/datasetRouter.js
+++ web/back/routes/datasetRouter.js
@@ -4,6 +4,7 @@ const datasetController = require('../controllers/datasetController');
 const router = Router();
 router.get('/:dataset/info', datasetController.info)
 router.get('/:dataset/stats', datasetController.stats)
+router.get('/pacientstats', datasetController.pacients_stats)
 router.get('/all', datasetController.all)
 
 
