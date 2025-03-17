a15caccfe4815299aea767a50bcfc6dbd3b5a0e5
Felipe Gauer <felipe.gattelli@edu.pucrs.br>
2025-03-17 17:23:20 -0300

    final deploy 2

--- web/back/index.js
+++ web/back/index.js
@@ -22,9 +22,6 @@ app.use('/api/user',express.json() ,userRouter);
 app.use('/api/pacient',express.json() ,authenticateToken,pacientRouter);
 app.use('/api/info',express.json() ,infoRouter);
 app.use('/api/dataset',express.json(), authenticateToken , datasetRouter);//app.use('/dataset',express.json() ,authenticateToken, datasetRouter);
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 db.once('open',()=>{console.log("Connected to MongoDb");});
 app.listen(process.env.PORT, ()=>console.log("Server running on port " + process.env.PORT));
