9a288da9e3827a2121fe8992eba9d13248bcbb7f
Felipe Gauer <felipe.gattelli@edu.pucrs.br>
2025-03-18 14:43:01 -0300

    test

--- web/back/index.js
+++ web/back/index.js
@@ -7,6 +7,7 @@ const datasetRouter = require('./routes/datasetRouter')
 const authenticateToken = require('./auth/authMiddleware')
 const mongoose = require('mongoose')
 const cors = require('cors');
+const path = require('path')
 
 const app = express()
 
@@ -20,11 +21,15 @@ app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI).catch((err)=>{console.log(err);});
 const db = mongoose.connection;
+app.use(express.static(path.join(__dirname, './public')))
 
 app.use('/api/user',express.json() ,userRouter);
 app.use('/api/pacient',express.json() ,authenticateToken,pacientRouter);
 app.use('/api/info',express.json() ,infoRouter);
 app.use('/api/dataset',express.json(), authenticateToken , datasetRouter);//app.use('/dataset',express.json() ,authenticateToken, datasetRouter);
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 db.once('open',()=>{console.log("Connected to MongoDb");});
 app.listen(process.env.PORT, ()=>console.log("Server running on port " + process.env.PORT));
